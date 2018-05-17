0a548f8ed2ccefe4a6b5de1d1a4787298eeb249b
a632079 <a632079@qq.com>
2018-05-17 20:03:18 +0800

    Add Copyright Information

--- src/preStart.js
+++ src/preStart.js
@@ -33,8 +33,30 @@ function setupWinston (logFile) {
   })
 }
 
+function randomHelloWord () {
+  const sentences = [
+    '我们一路奋战，不是为了改变世界，而是为了不让世界改变我们。',
+    '多一分心力去注意别人，就少一分心力反省自己，你懂吗？',
+    '老是低着头，幸福可是会离你而去的喔。',
+    '能够实现愿望的樱花树啊，为什么不能满足这些孩子们如此真诚的要求呢？',
+    '到底该怎么做，你才会重新喜欢我呢？'
+  ]
+  return sentences[Math.floor(Math.random() * sentences.length)]
+}
+
+function printCopyright () {
+  const colors = require('colors/safe')
+  const pkg = require('../package')
+  const date = new Date()
+  console.log(colors.bgBlue(colors.black(' ' + pkg.name + ' v' + pkg.version + ' © ' + date.getFullYear() + ' All Rights Reserved. ')) + '   ' + colors.bgRed(colors.black(' Contact at a632079@qq.com ')))
+  console.log('')
+  console.log(colors.bgCyan(colors.black(` ${randomHelloWord()} `)))
+}
+
 module.exports = {
   preStart: (logFile) => {
+    printCopyright()
+
     // 检测 Node.JS 版本
     if (parseInt(process.version.slice(1, process.version.length)) < 8) {
       winston.error(`本脚本使用了 ES2017 的 Async Function, 所以要求 Node.js 版本大于 8. 您目前安装的 Node.js 版本为: ${process.version}`)
